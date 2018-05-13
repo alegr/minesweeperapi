11ec9db296285717a7bdc445dba8ed1bca7b09a0
Alejandro Garcia del Rio <alejandro.garciadelrio@gmail.com>
2018-05-13 20:44:32 -0300

    add method for listing games

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -7,4 +7,19 @@ use Illuminate\Support\Facades\Storage;
 
 class GamesController extends Controller
 {
+
+    /**
+     * Return a list of all games.
+     *
+     * @return array
+     */
+    public function index(Request $request)
+    {
+        $games = \App\Models\Game::all();
+        return response()->json([
+            'success' => true,
+            'data' => $games,
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List all games
+Route::get('/games', 'GamesController@index');
